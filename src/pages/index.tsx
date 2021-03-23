981423bd6c9c23234bb77029c87a3d81e94f42cd
Ivan Nikitin <nikitin.ivan.dev@gmail.com>
2021-03-23 13:11:05 +0000

    Add conditional render on landing page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,23 +1,33 @@
-import { Box, Flex, Heading } from "@chakra-ui/core";
+import { Box, Flex, Heading, Text } from "@chakra-ui/core";
 import { withUrqlClient } from "next-urql";
 import React from "react";
 import Footer from "../components/Footer";
 import { Layout } from "../components/Layout";
+import { useMeQuery } from "../generated/graphql";
 import Workspace from "../public/workspace.svg";
 import { createUrqlClient } from "../urql/createUrqlClient";
 
 const Index = () => {
+  const [{ data }] = useMeQuery();
+  let body = null;
+  if (!data?.me) {
+    body = (
+      <Flex alignItems="flex-end" justify="space-evenly" ml={10} mt={10}>
+        <Heading as="h1" size="2xl" letterSpacing={"-.1rem"} ml={1}>
+          Keep track of anything
+        </Heading>
+        <Box size="2xl">
+          <Workspace />
+        </Box>
+      </Flex>
+    );
+  } else {
+    body = <Text>Under development</Text>;
+  }
   return (
     <>
       <Layout />
-      <Flex alignItems="flex-end" justify="space-evenly" ml={10} mt={10}>
-          <Heading as="h1" size="2xl" letterSpacing={"-.1rem"} ml={1}>
-            Keep track of anything
-          </Heading>
-          <Box size="2xl">
-            <Workspace/>
-          </Box>
-      </Flex>
+      {body}
       <Footer />
     </>
   );
