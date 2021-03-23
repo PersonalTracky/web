4baa2440d5cd6a686a1a9f844892ea47bb03e186
Ivan Nikitin <nikitin.ivan.dev@gmail.com>
2021-03-23 13:07:42 +0000

    Add basic landing page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,23 @@
+import { Box, Flex, Heading } from "@chakra-ui/core";
 import { withUrqlClient } from "next-urql";
 import React from "react";
 import Footer from "../components/Footer";
 import { Layout } from "../components/Layout";
+import Workspace from "../public/workspace.svg";
 import { createUrqlClient } from "../urql/createUrqlClient";
 
 const Index = () => {
   return (
     <>
       <Layout />
+      <Flex alignItems="flex-end" justify="space-evenly" ml={10} mt={10}>
+          <Heading as="h1" size="2xl" letterSpacing={"-.1rem"} ml={1}>
+            Keep track of anything
+          </Heading>
+          <Box size="2xl">
+            <Workspace/>
+          </Box>
+      </Flex>
       <Footer />
     </>
   );
